daab6f7a23c072838d30314befdaddbe05e35dc1
Karthik <karthik546712@gmail.com>
2026-07-20 11:06:39 +0530

    Updated database connection for cloud

--- backend/index.js
+++ backend/index.js
@@ -5,8 +5,7 @@ const pool = require('./db'); // Imports your database connection
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const cors = require('cors');
-app.use(cors());
+
 
 app.use(cors());
 app.use(express.json());
