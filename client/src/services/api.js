8e3496ea6bdeb2eeceb85b8adfe1159816000fc0
Karthik <karthik546712@gmail.com>
2026-07-31 08:32:04 +0530

    Updated production API URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -4,7 +4,7 @@ const API = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:5000/api"
-      : "https://YOUR-RENDER-BACKEND.onrender.com/api",
+      : "https://future-fs-02-backend-w1yo.onrender.com/api",
 
   headers: {
     "Content-Type": "application/json",
